3c9409dbb960a7ab88f086a7c999f961c1a28ae9
mgkshruti <sruthim.ss08@gmail.com>
2017-08-20 12:37:06 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function(req,res){
     res.sendFile('Article one requested and will be served here');
-})
+});
 
 app.get('/article-two',function(req,res){
     res.sendFile('Article two requested and will be served here');
-})
+});
 
 app.get('/article-three',function(req,res){
     res.sendFile('Article three requested and will be served here');
-})
+});
 
 
 
